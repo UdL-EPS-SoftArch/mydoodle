2bb15f65b0b98ec8107cdb51ee2473791aa82a54
Kevin Borrell <kborrells@gmail.com>
2015-10-27 02:17:21 +0100

    Implemented view of meeting proposal creation form.

--- yo/app/scripts/controllers/meetingcreatecontroller.js
+++ yo/app/scripts/controllers/meetingcreatecontroller.js
@@ -1,16 +1,13 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name webappApp.controller:MeetingcreatecontrollerCtrl
- * @description
- * # MeetingcreatecontrollerCtrl
- * Controller of the webappApp
- */
 angular.module('webappApp')
-  .controller('MeetingCreateCtrl', function ($scope, MeetingProposal) {
+  .controller('MeetingCreateController', function ($scope, MeetingProposal) {
         $scope.meeting = {};
         $scope.addMeeting = function () {
             $scope.meeting = {};
+            $scope.createform.$setPristine();
+            $scope.createform.$setValidity();
+            $scope.createform.$setUntouched();
+            $scope.apply();
         }        
   });
