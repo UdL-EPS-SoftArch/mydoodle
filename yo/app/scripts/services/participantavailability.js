49584a4ae8cea868d21eb8554084928f8970aff4
Muahahas <eloibuisan@gmail.com>
2015-11-03 16:53:08 +0100

    Inici part client send meeting

--- yo/app/scripts/services/participantavailability.js
+++ yo/app/scripts/services/participantavailability.js
@@ -1,12 +1,13 @@
 'use strict';
 
+
 angular.module('webappApp')
   .factory('ParticipantAvailability', ['$resource', function($resource) {
-    return $resource('http://localhost:8080/api/participantAvailability/:id', null,
-    //return $resource('http://localhost:8080/api/meetingProposals/:id/availabilities', null,
+    return $resource('/api/participantAvailability/:id', null,
       {
         'query': { method:'GET', isArray: false },
         'update': { method:'PUT' }
       });
   }]);
+
 
