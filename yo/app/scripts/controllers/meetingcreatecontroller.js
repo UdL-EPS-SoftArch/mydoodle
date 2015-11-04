8524aca987249041e7002ddb57f7cc22bb862aef
Kevin Borrell <kborrells@gmail.com>
2015-11-04 16:31:47 +0100

    Added loading screen when creating a meeting proposal.

--- yo/app/scripts/controllers/meetingcreatecontroller.js
+++ yo/app/scripts/controllers/meetingcreatecontroller.js
@@ -4,9 +4,12 @@ angular.module('webappApp')
   .controller('MeetingCreateController', function ($scope, MeetingProposal) {
         $scope.meeting = {};
         $scope.created = false;
+        $scope.creating = false;
         $scope.adminlink = "";
         $scope.addMeeting = function () {
+            $scope.creating = true;
             MeetingProposal.save($scope.meeting).$promise.then(function (meeting) {
+                $scope.creating = false;
                 $scope.created = true;
                 $scope.adminlink = meeting.adminKey;
             });
