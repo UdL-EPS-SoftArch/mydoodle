d7f5df570c19c89615055e612b896bc4162a3169
Muahahas <eloibuisan@gmail.com>
2015-11-05 16:50:58 +0100

    Rebase amb development i solucionat problema.

--- yo/app/scripts/controllers/addparticipantscontroller.js
+++ yo/app/scripts/controllers/addparticipantscontroller.js
@@ -1,54 +1,19 @@
 'use strict';
 
 angular.module('webappApp')
-  .controller('ParticipantAddController', function ($scope, $stateParams, MeetingProposal, ParticipantAvailability) {
-
-    $scope.meetingId = $stateParams.id;
-    $scope.adminKey = "";
-    if($stateParams.adminKey!=null) {
-      $scope.finalAdminKey = $stateParams.adminKey;
-    }
-    $scope.emails=[];
-
-
-    $scope.participants = [];
-
-    $scope.addKey = function () {
-      MeetingProposal.get({id: $scope.meetingId, key: $scope.adminKey})
-        .$promise.then(function (meeting) {
-          $scope.meeting = meeting;
-          $scope.finalAdminKey = $scope.adminKey;
-          $scope.adminKey = '';
-          $scope.participantsSaved=$scope.meeting.availabilities;
-        });
-    };
-
+  .controller('ParticipantAddController', function ($scope) {
+    $scope.participants=[];
     $scope.addParticipant = function () {
-      if ($scope.participant != "" && $scope.emails.indexOf($scope.participant.participant) == -1 && $scope.finalAdminKey != null) {
+      if($scope.participants.indexOf($scope.participant)==-1){
         $scope.participants.push($scope.participant);
-        $scope.emails.push($scope.participant.participant);
       }
-      $scope.participant = '';
+      $scope.participant='';
     };
-
-    $scope.removeParticipant = function (index) {
-      $scope.participants.splice(index, 1);
+    $scope.removeParticipant = function(index){
+      $scope.participants.splice(index,1);
     };
+    $scope.submitParticipants = function(){
 
-    $scope.submitParticipants = function () {
-      for(var i=0;i<$scope.participants.length;i++) {
-        ParticipantAvailability.save($scope.participants[i]).$promise.then(function (participant) {
-          //alert(participant.participant);
-        });
-      }
-
-      /*MeetingProposal.get({id: $scope.meetingId, key: $scope.finalAdminKey})
-        .$promise.then(function (meeting) {
-          $scope.meeting = meeting;
-          $scope.participantsSaved=$scope.meeting.availabilities;
-        });*/
-
-      $scope.participants = [];
-      $scope.emails = [];
+      $scope.participants=[];
     };
   });
