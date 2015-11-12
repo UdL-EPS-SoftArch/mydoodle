56d5122d9e0a48b8b7f1c0d5d30db4c91e829e49
xavics <xacosa@gmail.com>
2015-11-12 10:41:05 +0100

    Afegit un modal de prova

--- yo/app/scripts/controllers/addtimeslotcontroller.js
+++ yo/app/scripts/controllers/addtimeslotcontroller.js
@@ -2,36 +2,11 @@
  * Created by cobos on 05/11/2015.
  */
 angular.module('webappApp')
-  .controller('AddTimeSlotController', function($scope, $uibModalInstance, TimeSlots, meetingProposalId) {
+  .controller('AddTimeSlotController', function($scope, $uibModalInstance, meetingProposalId) {
     $scope.meetingProposalId = meetingProposalId;
-    $scope.minDate = new Date();
-    $scope.maxDate = new Date();
-    $scope.maxDate.setFullYear($scope.maxDate.getFullYear()+5);
-
-    $scope.dateOptions = {
-      formatYear: 'yy',
-      startingDay: 1
-    };
-
-    $scope.format = 'dd-MMMM-yyyy';
-
-    $scope.time = new Date();
-    $scope.time.setSeconds(0);
-    $scope.time.setMilliseconds(0);
 
     $scope.ok = function () {
-      var timeSlot = new TimeSlots();
-      timeSlot.dateTime = $scope.time.toJSON();
-      timeSlot.meeting = "http://localhost:8080/api/meetingProposals/"+meetingProposalId;
-      //var response = timeSlot.$save().$promise.then(function(timeSlotRes) {
-      //  $scope.user = user;
-      //});
-      timeSlot.$save(timeSlot, function(timeSlotResp){
-        $scope.responseTimeSlot = timeSlotResp;
-        $uibModalInstance.close($scope.responseTimeSlot);
-      });
-      //var response = timeSlot.$save().$promise;
-      //debugger;
+      $uibModalInstance.close();
     };
 
     $scope.cancel = function () {
--- yo/app/scripts/controllers/meetingviewcontroller.js
+++ yo/app/scripts/controllers/meetingviewcontroller.js
@@ -10,11 +10,70 @@
  * Controller of the webappApp
  */
 angular.module('webappApp')
-  .controller('MeetingViewController', function($scope, $stateParams, MeetingProposal) {
+  .controller('MeetingViewController', function($scope, $stateParams, MeetingProposal, $uibModal) {
     $scope.key = $stateParams.key;
     $scope.meetingId = $stateParams.id;
     MeetingProposal.query($stateParams.id, $stateParams.key)
       .$promise.then(function (meeting) {
         $scope.meeting = meeting;
+        $scope.slotsCalendar = {};
+        for(var x=0; x<meeting.slots.length; x++){
+          debugger;
+          var date = new Date($scope.meeting.slots[x].dateTime);
+          var year = date.getFullYear();
+          var month = date.getMonth();
+          var day = date.getDate();
+          var hour = date.getHours();
+          if($scope.slotsCalendar.years){
+            for(var y=0; y<$scope.slotsCalendar.years.length; y++){
+              if($scope.slotsCalendar.years[y].value != year){
+                $scope.slotsCalendar.years.push({'value': year, 'months': [{'value': month, 'days': [{'value': day, 'hours': [{'value': hour}]}]}]});
+                break;
+              }
+              else{
+                for(var m=0; m<$scope.slotsCalendar.years[y].months.length; m++){
+                  if($scope.slotsCalendar.years[y].months[m].value != month){
+                    $scope.slotsCalendar.years[y].months.push({'value': month, 'days': [{'value': day, 'hours': [{'value': hour}]}]});
+                    break;
+                  }
+                  else{
+                    for(var d=0; d<$scope.slotsCalendar.years[y].months[m].days.length; d++){
+                      if($scope.slotsCalendar.years[y].months[m].days[d].value != day){
+                        $scope.slotsCalendar.years[y].months[m].days.push({'value': day, 'hours': [{'value': hour}]});
+                        break;
+                      }else{
+                        for(var h=0; h<$scope.slotsCalendar.years[y].months[m].days[d].hours.length; h++){
+                          if($scope.slotsCalendar.years[y].months[m].days[d].hours[h].value != hour){
+                            $scope.slotsCalendar.years[y].months[m].days[d].hours.push({'value': hour});
+                            break;
+                          }else{
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }else{
+              $scope.slotsCalendar.years = [{'value': year, 'months': [{'value': month, 'days': [{'value': day, 'hours': [{'value': hour}]}]}]}]
+            }
+        }
       });
+    $scope.newTimeSlot = function (){
+      var modalInstance = $uibModal.open({
+        animation: true,
+        templateUrl: 'views/addtimeslotmodal.html',
+        controller: 'AddTimeSlotController',
+        size: 'lg',
+        resolve: {
+          meetingProposalId: function () {
+            return $scope.meetingId;
+          }
+        }
+      });
+      modalInstance.result.then(function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
   });
