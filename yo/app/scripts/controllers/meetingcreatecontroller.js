eec8064abcce4a171a81258f5a170e1bec574ec0
Kevin Borrell <kborrells@gmail.com>
2015-11-19 17:47:02 +0100

    Fixed meeting proposal view url, which now shows the ID. URL host and port are set up dynamically. Modified visual style of webpage and form.

--- yo/app/scripts/controllers/meetingcreatecontroller.js
+++ yo/app/scripts/controllers/meetingcreatecontroller.js
@@ -1,17 +1,20 @@
 'use strict';
 
 angular.module('webappApp')
-  .controller('MeetingCreateController', function ($scope, MeetingProposal) {
+  .controller('MeetingCreateController', function ($scope, $location, MeetingProposal) {
+        $scope.location = $location;
         $scope.meeting = {};
         $scope.created = false;
         $scope.creating = false;
         $scope.adminlink = "";
+        $scope.id = "";
         $scope.addMeeting = function () {
             $scope.creating = true;
             MeetingProposal.save($scope.meeting).$promise.then(function (meeting) {
                 $scope.creating = false;
                 $scope.created = true;
                 $scope.adminlink = meeting.adminKey;
+                $scope.id = meeting.id;
             });
             $scope.meeting = {};
             $scope.createform.$setPristine();
