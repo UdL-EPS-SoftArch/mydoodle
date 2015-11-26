5659cf7bff5b537af65bb6bbb051b2e4ee20f035
Muahahas <eloibuisan@gmail.com>
2015-11-26 16:46:45 +0100

    Desde la llista de meetings es poden afegir participants. Falta mostrar els participants ja guardats i desar els que s'han d'afegir.

--- yo/app/scripts/controllers/addparticipantscontroller.js
+++ yo/app/scripts/controllers/addparticipantscontroller.js
@@ -1,19 +1,36 @@
 'use strict';
 
 angular.module('webappApp')
-  .controller('ParticipantAddController', function ($scope) {
-    $scope.participants=[];
+  .controller('ParticipantAddController', function ($scope,$stateParams){//,MeetingProposal,ParticipantAvailability) {
+
+    $scope.meetingId = $stateParams.id;
+
+   /* MeetingProposal.query({id:$scope.meetingId,key:$scope.adminKey})
+      .$promise.then(function (meeting) {
+        $scope.meeting = meeting._embeddedItems;
+        $scope.title = $scope.meeting.title;
+      });*/
+
+
+
+    $scope.participants = [];
+
     $scope.addParticipant = function () {
-      if($scope.participants.indexOf($scope.participant)==-1){
+      if ($scope.participants.indexOf($scope.participant) == -1) {
         $scope.participants.push($scope.participant);
       }
-      $scope.participant='';
+      $scope.participant = '';
     };
-    $scope.removeParticipant = function(index){
-      $scope.participants.splice(index,1);
+    $scope.removeParticipant = function (index) {
+      $scope.participants.splice(index, 1);
     };
-    $scope.submitParticipants = function(){
 
-      $scope.participants=[];
+    $scope.submitParticipants = function () {
+     /* for(i=0;i<$scope.participants.length;i++){
+        ParticipantAvailability.save($scope.participants[i]).$promise.then(function (partcipants) {
+          $scope.participants.splice(i,1);
+        });
+      }*/
+      $scope.participants = [];
     };
   });
