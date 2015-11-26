126d0af6aa8b21a746272a5f99a95a2406ae4635
Muahahas <eloibuisan@gmail.com>
2015-11-26 16:46:45 +0100

    Desde la llista de meetings es poden afegir participants. Falta mostrar els participants ja guardats i desar els que s'han d'afegir.

--- yo/app/scripts/services/participantavailability.js
+++ yo/app/scripts/services/participantavailability.js
@@ -2,7 +2,8 @@
 
 angular.module('webappApp')
   .factory('ParticipantAvailability', ['$resource', function($resource) {
-    return $resource('/api/participantAvailability/:id', null,
+    return $resource('http://localhost:8080/api/participantAvailability/:id', null,
+    //return $resource('http://localhost:8080/api/meetingProposals/:id/availabilities', null,
       {
         'query': { method:'GET', isArray: false },
         'update': { method:'PUT' }
