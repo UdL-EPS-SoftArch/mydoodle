aa38fe6b54aba733209292a324c97f1cc8fa4a62
Muahahas <eloibuisan@gmail.com>
2015-12-01 15:31:52 +0100

    Es guarda en el servidor el participant i s'accedeix al meeting, falta desar el  meeting al participant.

--- yo/app/scripts/services/participantavailability.js
+++ yo/app/scripts/services/participantavailability.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 angular.module('webappApp')
   .factory('ParticipantAvailability', ['$resource', function($resource) {
     return $resource('/api/participantAvailability/:id', null,
