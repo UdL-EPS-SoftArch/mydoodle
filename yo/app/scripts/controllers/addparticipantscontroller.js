0e3674eb6774a9bd424883f5af01b795f6cad499
Muahahas <eloibuisan@gmail.com>
2015-12-01 15:31:52 +0100

    Es guarda en el servidor el participant i s'accedeix al meeting, falta desar el  meeting al participant.

--- yo/app/scripts/controllers/addparticipantscontroller.js
+++ yo/app/scripts/controllers/addparticipantscontroller.js
@@ -1,19 +1,51 @@
 'use strict';
 
 angular.module('webappApp')
-  .controller('ParticipantAddController', function ($scope) {
-    $scope.participants=[];
-    $scope.addParticipant = function () {
-      if($scope.participants.indexOf($scope.participant)==-1){
-        $scope.participants.push($scope.participant);
-      }
-      $scope.participant='';
-    };
-    $scope.removeParticipant = function(index){
-      $scope.participants.splice(index,1);
-    };
-    $scope.submitParticipants = function(){
+  .controller('ParticipantAddController', function ($scope, $stateParams, MeetingProposal, ParticipantAvailability) {
 
-      $scope.participants=[];
+    $scope.meetingId = $stateParams.id;
+    $scope.adminKey = "";
+    $scope.emails=[];
+
+
+    $scope.participants = [];
+
+    $scope.addKey = function () {
+      MeetingProposal.get({id: $scope.meetingId, key: $scope.adminKey})
+        .$promise.then(function (meeting) {
+          $scope.meeting = meeting;
+          $scope.finalAdminKey = $scope.adminKey;
+          $scope.adminKey = '';
+          $scope.participantsSaved=$scope.meeting.availabilities;
+        });
+    }
+
+    $scope.addParticipant = function () {
+      if ($scope.participant != "" && $scope.emails.indexOf($scope.participant.participant) == -1 && $scope.finalAdminKey != null) {
+        $scope.participants.push($scope.participant);
+        $scope.emails.push($scope.participant.participant);
+      }
+      $scope.participant = '';
+    };
+
+    $scope.removeParticipant = function (index) {
+      $scope.participants.splice(index, 1);
+    };
+
+    $scope.submitParticipants = function () {
+      for(var i=0;i<$scope.participants.length;i++) {
+        ParticipantAvailability.save($scope.participants[i]).$promise.then(function (participant) {
+          //alert(participant.participant);
+        });
+      }
+
+      /*MeetingProposal.get({id: $scope.meetingId, key: $scope.finalAdminKey})
+        .$promise.then(function (meeting) {
+          $scope.meeting = meeting;
+          $scope.participantsSaved=$scope.meeting.availabilities;
+        });*/
+
+      $scope.participants = [];
+      $scope.emails = [];
     };
   });
