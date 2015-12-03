e6bd0009e3c512a6538557ec9ee02caec56f28b8
Roberto García <roberto@rhizomik.net>
2015-12-03 11:05:09 +0100

    [Delivers #109430500] Implemented Delete meeting button

--- yo/app/scripts/controllers/meetingslistcontroller.js
+++ yo/app/scripts/controllers/meetingslistcontroller.js
@@ -13,9 +13,18 @@ angular.module('webappApp')
       .$promise.then(function (meetings) {
         $scope.meetings = meetings._embeddedItems;
       });
+
     $scope.onClickView = function (meeting){
-      if(meeting.key){
+      if(meeting.key)
         $state.go('viewMeeting', {'id': meeting.id, 'key': meeting.key})
-      }
+    };
+
+    $scope.deleteMeeting = function (meeting){
+      if(meeting.key)
+        MeetingProposal.delete({ id: meeting.id, key: meeting.key }, function() {
+          $scope.meetings = $scope.meetings.filter(function (item) {
+            return item.id !== meeting.id;
+          })
+        });
     };
   });
