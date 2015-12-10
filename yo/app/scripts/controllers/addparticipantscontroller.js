654ef9f6f6baa3a0fd3edd1c9931cd38582e1814
Roberto García <roberto@rhizomik.net>
2015-12-10 15:28:01 +0100

    [#104350032] Fix issues due to rebase and bugs in the AngularJS client

--- yo/app/scripts/controllers/addparticipantscontroller.js
+++ yo/app/scripts/controllers/addparticipantscontroller.js
@@ -1,14 +1,20 @@
 'use strict';
 
 angular.module('webappApp')
-  .controller('ParticipantAddController', function ($scope) {
+  .controller('ParticipantAddController', function ($scope, $state, $stateParams, ParticipantAvailability) {
     $scope.participants=[];
+    $scope.finalAdminKey = $stateParams.key;
+    $scope.meetingId = $stateParams.id;
+
+    $scope.participant = new ParticipantAvailability();
+    $scope.participant.meeting = "/meetingProposals/"+$scope.meetingId;
 
     $scope.addParticipant = function () {
       if($scope.participants.indexOf($scope.participant)==-1){
         $scope.participants.push($scope.participant);
       }
-      $scope.participant='';
+      $scope.participant = new ParticipantAvailability();
+      $scope.participant.meeting = "/meetingProposals/"+$scope.meetingId;
     };
 
     $scope.removeParticipant = function(index){
@@ -16,12 +22,13 @@ angular.module('webappApp')
     };
 
     $scope.submitParticipants = function(){
-      for(var i=0;i<$scope.participants.length;i++)
+      for(var i=0;i<$scope.participants.length;i++) {
         ParticipantAvailability.save($scope.participants[i]).
-          $promise.then(function (participant) {
-            $scope.participants = $scope.participants.filter(function(item) {
-              return item.id !== participant.id;
-            })
-          });
+        $promise.then(function (participant) {
+          $scope.participants = $scope.participants.filter(function (item) {
+            return item.participant !== participant.participant;
+          })
+        });
+      }
     };
   });
