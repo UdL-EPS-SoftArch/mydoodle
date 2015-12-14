7c632b91feb9f2d23cf4986e64232522942ca1ed
Kevin Borrell <kborrells@gmail.com>
2015-12-14 20:20:56 +0100

    Implemented "Next step" button functionality.

--- yo/app/scripts/controllers/meetingcreatecontroller.js
+++ yo/app/scripts/controllers/meetingcreatecontroller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('webappApp')
-    .controller('MeetingCreateController', function ($scope, $location, MeetingProposal) {
+    .controller('MeetingCreateController', function ($scope, $state, $location, MeetingProposal) {
         $scope.location = $location;
         $scope.meeting = {};
         $scope.createState = 0; // 0->Not created 1->Creating 2->Created -1 -> ERROR
@@ -24,6 +24,10 @@ angular.module('webappApp')
         $scope.reset = function () {
           $scope.createState = 0;
           $state.go("newMeeting");
-        }
+        };
+
+        $scope.goToAddTimeSlots = function () {
+          $location.url("/meetings/" + $scope.id + "?key=" +  $scope.adminlink);
+        };
     });
 
