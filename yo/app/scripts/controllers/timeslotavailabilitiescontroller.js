64c2ff7f043e69bc8866581b5b198a0c693696c7
millanes4 <millanesporter@gmail.com>
2016-01-07 16:45:36 +0100

    added query on avaliability timeSlot Controller

--- yo/app/scripts/controllers/timeslotavailabilitiescontroller.js
+++ yo/app/scripts/controllers/timeslotavailabilitiescontroller.js
@@ -7,20 +7,14 @@ angular.module('webappApp')
   .controller('TimeSlotAvailabilitiesController', function($scope, $uibModalInstance, participantAvailabilityId,
                                                            TimeSlotAvailability, $stateParams,  TimeSlots ) {
 
-    $scope.participantID = $stateParams.id
-    $scope.participantmeeting= "/participantAvailabilities/" + $scope.participantID + "/meeting"
-    $scope.meetingID= $scope.participantmeeting.id
-    $scope.slotAvailability = "/meetingProposals/"+ $scope.meetingID + "/slots"
+    $scope.meetingAvailability = TimeSlots.searchbymeeting
 
-
-   // $scope.participantAvailabilityId = participantAvailability;
-   // $scope.TimeSlotAvailabilityId = TimeSlotAvailability;
-    //$scope.TimeSlotsId = TimeSlots;
-
-    //$scope.participantAvailabilityId.slotAvailabilities = "timeSlots/" + $scope.TimeSlotsId;
-    //$scope.participantAvailabilityId.slotAvailabilities.timeSlotAvailabilities = "timeSlotsAvailabilities/"
-                                                                                        +$scope.TimeSlotAvailabilityId;
-
+    participantAvailabilityId.query({id:$stateParams.id })
+      .promise.then(function (participant){
+        $scope.participant = participant;
+        $scope.meetingAvailability = TimeSlots.searchbymeeting(participant.meeting.id)
+    });
+    
     //// aixo encara no
     $scope.ok = function () {
       var timeSlotAvailability = new TimeSlotAvailability();
