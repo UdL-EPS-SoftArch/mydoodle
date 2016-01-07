a46c1656cd6dcff5075417d140753c31a66d4ab9
Roberto García <roberto@rhizomik.net>
2016-01-07 21:01:38 +0100

    Fixed bug in splice, it return and array so before pushing into the other array we need to get the element from it.

--- yo/app/scripts/controllers/deletemeetingparticipantcontroller.js
+++ yo/app/scripts/controllers/deletemeetingparticipantcontroller.js
@@ -9,30 +9,20 @@ angular.module('webappApp')
     $scope.finalAdminKey = $stateParams.key;
     $scope.meetingId = $stateParams.id;
 
-    $scope.participant = new ParticipantAvailability();
-    $scope.participant.meeting = "/meetingProposals/" + $scope.meetingId;
     $scope.selected = [];
 
     $scope.deleteParticipant = function (index) {
-      $scope.participantsdeleted.push($scope.availabilities.splice(index, 1));
-
-
+      $scope.participantsdeleted.push($scope.availabilities.splice(index, 1)[0]);
     };
 
-
-
     $scope.submitParticipants = function () {
-      for (var i = 0; i < $scope.participantsdeleted.length; i++) {
-        alert($scope.participantsdeleted[i]);
-        $scope.participantsdeleted[i].$remove;
+      for (var i = 0; i < $scope.participantsdeleted.length; i++)
+        ParticipantAvailability.delete({'id': $scope.participantsdeleted[i].id});
+    };
 
-      }};
     MeetingProposal.query({id: $stateParams.id, key: $stateParams.key})
       .$promise.then(function (meeting) {
         $scope.availabilities = meeting.availabilities;
-
-      });;
-
-
+      });
 
   });
